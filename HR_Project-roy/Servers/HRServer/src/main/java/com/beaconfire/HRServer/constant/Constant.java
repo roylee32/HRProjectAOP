7359d639dee01605d6d00a89e24f377c36386cc2
Roy Lee <roylee@Roys-MBP.lan>
2021-04-02 03:00:02 -0400

    Unit Testing and Logging

--- HR_Project-roy/Servers/HRServer/src/main/java/com/beaconfire/HRServer/constant/Constant.java
+++
@@ -1,6 +0,0 @@
-package com.beaconfire.HRServer.constant;
-
-public interface Constant {
-    String JWT_TOKEN_COOKIE_NAME = "JWT";
-    String SIGNING_KEY = "Group_4_Signing_key";
-}
--- HR_Project-roy/Servers/HRServer/src/main/java/com/beaconfire/HRServer/exception/EmploymentStartDateNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.beaconfire.HRServer.exception;
-
-import com.beaconfire.HRServer.domain.Employee;
-
-public class EmploymentStartDateNotFoundException extends Exception{
-    public EmploymentStartDateNotFoundException(String s){
-        super(s);
-    }
-}
